65d34cf401ad7fe9b4ad75dba24ad86d9eb4f490
phe0 <pheofo@gmail.com>
2020-10-30 12:02:01 -0300

    add grammar

--- parser.py
+++ parser.py
@@ -7,9 +7,37 @@ class Symbol(NamedTuple):
     value: str
 
 
-grammar = Lark(
-    r"""
-start : "implemente-aqui!"
+grammar = Lark(r"""
+    start : expr
+
+    ?expr  : atom
+           | func
+           | list
+           | quoted
+
+    quoted : "'" expr
+
+    func   : "(" SYMBOL expr+ ")"
+           | "(" NAME expr+ ")"
+
+    list   : "(" expr+ ")"
+
+
+    ?atom  : STRING
+           | SYMBOL
+           | NUMBER
+           | BOOLEAN
+           | NAME
+           | CHAR
+
+    STRING : /"([^"\\\n\r\b\f]+|\\["\\\/bfnrt]|\\u[0-9a-fA-F]{4})*"/
+    SYMBOL: /[-+=\/*!@$^&~<>?]+/
+    NUMBER : /-?\d+(\.\d+)?/
+    BOOLEAN: /\#t|\#nil/
+    NAME   : /[a-zA-Z][-?\w]*/
+    CHAR   : /\#[\w\\]+/
+    %ignore /\s+/
+    %ignore /;[^\n]*/
 """)
 
 
@@ -26,3 +54,27 @@ class LispyTransformer(InlineTransformer):
         "space": " ",
         "tab": "\t",
     }
+    def string(self, token):
+        return eval(token)
+
+    # def number(self, token):
+    #     return float(token)
+    
+    # def boolean(self, token):
+    #     return True if token.value == "#t" else False
+
+
+exprs = [
+    "((x 1))",
+    "((x 1) (y 2))",
+    "(let ((x 1) (y 2)) (+ x y))",
+    "((1 2 3) (3 2 1))",
+    "((diff cos) x)",
+    "(let 1 2)"
+]
+
+for src in exprs:
+    tree = grammar.parse(src)
+    print(src)
+    print(tree.pretty())
+    print('-' * 40)
