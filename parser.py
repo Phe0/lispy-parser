e04bdceef48941b94c71c91008f24337027d41b8
phe0 <pheofo@gmail.com>
2020-10-30 13:18:49 -0300

    insert transformer code cappable of passing the 11 fisrt tests

--- parser.py
+++ parser.py
@@ -8,34 +8,30 @@ class Symbol(NamedTuple):
 
 
 grammar = Lark(r"""
-    start : expr
+    ?start : expr+
 
     ?expr  : atom
-           | func
-           | list
-           | quoted
+           | lista
+           | quoted 
 
     quoted : "'" expr
 
-    func   : "(" SYMBOL expr+ ")"
-           | "(" NAME expr+ ")"
-
-    list   : "(" expr+ ")"
+    lista   : "(" expr+ ")"
 
 
-    ?atom  : STRING
-           | SYMBOL
-           | NUMBER
-           | BOOLEAN
-           | NAME
-           | CHAR
+    ?atom  : STRING -> string
+           | SYMBOL -> symbol
+           | NUMBER -> number
+           | BOOLEAN -> boolean
+           | NAME -> name
+           | CHAR -> char
 
-    STRING : /"([^"\\\n\r\b\f]+|\\["\\\/bfnrt]|\\u[0-9a-fA-F]{4})*"/
+    STRING : /"[^"\\]*"/
     SYMBOL: /[-+=\/*!@$^&~<>?]+/
     NUMBER : /-?\d+(\.\d+)?/
     BOOLEAN: /\#t|\#nil/
     NAME   : /[a-zA-Z][-?\w]*/
-    CHAR   : /\#[\w\\]+/
+    CHAR   : /\#\\\w+/
     %ignore /\s+/
     %ignore /;[^\n]*/
 """)
@@ -57,24 +53,40 @@ class LispyTransformer(InlineTransformer):
     def string(self, token):
         return eval(token)
 
-    # def number(self, token):
-    #     return float(token)
+    def number(self, token):
+        return float(token)
     
-    # def boolean(self, token):
-    #     return True if token.value == "#t" else False
+    def boolean(self, token):
+        return True if token.value == "#t" else False
 
+    def name(self, token):
+        return Symbol(token)
 
-exprs = [
-    "((x 1))",
-    "((x 1) (y 2))",
-    "(let ((x 1) (y 2)) (+ x y))",
-    "((1 2 3) (3 2 1))",
-    "((diff cos) x)",
-    "(let 1 2)"
-]
+    def quoted(self, token):
+        return [Symbol('quote'), token]
 
-for src in exprs:
-    tree = grammar.parse(src)
-    print(src)
-    print(tree.pretty())
-    print('-' * 40)
+    def lista(self, *args):
+        return list(args)
+
+    def char(self, token):
+        token = token.split('#\\')[-1]
+        return token if token.lower() not in self.CHARS else self.CHARS[token.lower()]
+
+    def start(self, *args):
+        array = list(args)
+        array.insert(0, Symbol('begin'))
+        return array
+
+# transformer = LispyTransformer()
+
+# exprs = [
+#     "(cmd 1)\n(cmd 2)"
+# ]
+
+# for src in exprs:
+#     print(src)
+#     tree = grammar.parse(src)
+#     print(tree)
+#     result = transformer.transform(tree)
+#     print(result)
+#     print('-' * 40)
